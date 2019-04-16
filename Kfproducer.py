c3c63bf01a9f78b0ea23631d809ba44edadae80a
Chris Green <39661130+CGreen4u@users.noreply.github.com>
2019-04-16 12:42:43 -0500

    Update Kfproducer.py

--- Kfproducer.py
+++ Kfproducer.py
@@ -11,17 +11,19 @@ class Kafka_Producer:
     def kf_produce(self, json_string):
         #turn the documnet into a json and then into bytes
         # string with encoding 'utf-8'
-        json_string = json.dumps(json_string)
-        fin_result = bytes(json_string, 'utf-8')
-    
-        #other producers below
-        #producer = KafkaProducer(bootstrap_servers='localhost:9092'
-        #plaintext used for docker
         producer = KafkaProducer(bootstrap_servers='PLAINTEXT://localhost:9092')
-        #extras host if needed
-        #producer2 = KafkaProducer(bootstrap_servers='localhost:9093')
-        #producer3 = KafkaProducer(bootstrap_servers='localhost:9094')
-        bracket = "{"
-        for _ in range(1):
-            print(producer.send('test', bytes(fin_result))) 
+        producer = KafkaProducer(value_serializer=lambda m: json.dumps(json_string).encode('ascii'))
+        future = producer.send('test', {'key': 'value'})
+
+         try:
+             record_metadata = future.get(timeout=10)
+         except KafkaError:
+             # Decide what to do if produce request failed...
+             log.exception()
+             pass
+
+          # Successful result returns assigned partition and offset
+          print (record_metadata.topic)
+          print (record_metadata.partition)
+          print (record_metadata.offset)
           
